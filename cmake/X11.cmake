aba0b88c1eb2bf0acb6fdb2b482588638692645c
Johnny Mast <mastjohnny@gmail.com>
2025-10-27 17:19:34 +0100

    Remove OpenCV_LIBS from target_link_libraries in X11.cmake

--- cmake/X11.cmake
+++ cmake/X11.cmake
@@ -3,5 +3,5 @@ if(CMAKE_SYSTEM_NAME STREQUAL "Linux" AND NOT WAYLAND)
 
     find_package(X11 REQUIRED)
 
-    target_link_libraries(${PROJECT_NAME} PRIVATE ${OpenCV_LIBS} X11 Xrandr Xtst)
+    target_link_libraries(${PROJECT_NAME} PRIVATE X11 Xrandr Xtst)
 endif()
