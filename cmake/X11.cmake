9b8a5f0d3c15c435a365fdcd03951f668cb4f194
Johnny Mast <mastjohnny@gmail.com>
2025-10-28 16:37:48 +0100

    Rename project from 'IO' to 'LibIO' in CMakeLists.txt for consistency with naming conventions.

--- cmake/X11.cmake
+++ cmake/X11.cmake
@@ -1,6 +1,4 @@
 if(CMAKE_SYSTEM_NAME STREQUAL "Linux" AND NOT WAYLAND)
-    message(STATUS "Configuring X11 and OpenCV for Linux (X11 backend)")
-
     find_package(X11 REQUIRED)
 
     target_link_libraries(${PROJECT_NAME} PRIVATE X11 Xrandr Xtst)
