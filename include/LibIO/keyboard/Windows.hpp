385b9e9aef4e3f66fbb98e63bf26ceed9120a7e5
Silentboy <mastjohnny@gmail.com>
2025-10-29 19:21:41 +0100

    Remove unused `Initialize` method from Windows keyboard class.

--- include/LibIO/keyboard/Windows.hpp
+++ include/LibIO/keyboard/Windows.hpp
@@ -9,10 +9,7 @@ namespace LibIO::Keyboard {
     class Windows final : public KeyboardControls {
     public:
         Windows() = default;
-
         ~Windows() override;
-
-        static void Initialize();
 
         void PressKey(const std::string &key) override;
 
