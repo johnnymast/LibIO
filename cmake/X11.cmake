38eaa17da5956dc92fa48a80d57ee616cdf8b3bb
Johnny Mast <mastjohnny@gmail.com>
2026-04-04 21:44:54 +0200

    Clean up redundant `target_include_directories` and `target_link_libraries` for Xtst in `X11.cmake`.

--- cmake/X11.cmake
+++ cmake/X11.cmake
@@ -5,9 +5,6 @@ if(CMAKE_SYSTEM_NAME STREQUAL "Linux")
     find_package(PkgConfig REQUIRED)
     pkg_check_modules(XTST REQUIRED xtst)
 
-    target_include_directories(${PROJECT_NAME} PUBLIC ${XTST_INCLUDE_DIRS})
-    target_link_libraries(${PROJECT_NAME} PUBLIC ${XTST_LIBRARIES})
-
     target_link_libraries(${PROJECT_NAME} PUBLIC X11 Xrandr ${XTST_LIBRARIES})
 endif()
 
