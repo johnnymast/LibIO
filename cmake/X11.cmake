ac600b67911e05a473cf45729f735348002ce8a7
Johnny Mast <mastjohnny@gmail.com>
2026-04-04 21:42:06 +0200

    Add Xrandr and Xtst dependencies in `X11.cmake` for Linux platform configuration

--- cmake/X11.cmake
+++ cmake/X11.cmake
@@ -1,6 +1,13 @@
 if(CMAKE_SYSTEM_NAME STREQUAL "Linux")
     find_package(X11 REQUIRED)
+    find_package(Xrandr REQUIRED)
 
-    target_link_libraries(${PROJECT_NAME} PRIVATE X11 Xrandr Xtst)
+    find_package(PkgConfig REQUIRED)
+    pkg_check_modules(XTST REQUIRED xtst)
+
+    target_include_directories(${PROJECT_NAME} PUBLIC ${XTST_INCLUDE_DIRS})
+    target_link_libraries(${PROJECT_NAME} PUBLIC ${XTST_LIBRARIES})
+
+    target_link_libraries(${PROJECT_NAME} PUBLIC X11 Xrandr ${XTST_LIBRARIES})
 endif()
 
