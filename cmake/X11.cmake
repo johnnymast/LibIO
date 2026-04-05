9dbc8328684a098c8ffe8874944bcdcc9bc5734a
Johnny Mast <mastjohnny@gmail.com>
2026-04-05 20:08:19 +0200

    Refactor `X11.cmake` to use private scoping for Xtst includes, library directories, and dependencies.

--- cmake/X11.cmake
+++ cmake/X11.cmake
@@ -5,6 +5,17 @@ if(CMAKE_SYSTEM_NAME STREQUAL "Linux")
     find_package(PkgConfig REQUIRED)
     pkg_check_modules(XTST REQUIRED xtst)
 
-    target_link_libraries(${PROJECT_NAME} PUBLIC X11 Xrandr ${XTST_LIBRARIES})
-endif()
+    target_include_directories(${PROJECT_NAME} PRIVATE
+            ${XTST_INCLUDE_DIRS}
+    )
 
+    target_link_directories(${PROJECT_NAME} PRIVATE
+            ${XTST_LIBRARY_DIRS}
+    )
+
+    target_link_libraries(${PROJECT_NAME} PRIVATE
+            X11::X11
+            X11::Xrandr
+            ${XTST_LIBRARIES}
+    )
+endif()
