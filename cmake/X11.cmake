db334945c2723ccb765179adcf2e36bb5ed66a6d
Johnny Mast <mastjohnny@gmail.com>
2026-04-05 20:46:20 +0200

    Refactor `X11.cmake` to include `Xrandr` via `pkg_check_modules`, switch `LibIO` scoping from PRIVATE to PUBLIC, and update module path in `CMakeLists.txt`.

--- cmake/X11.cmake
+++ cmake/X11.cmake
@@ -1,15 +1,19 @@
 if(CMAKE_SYSTEM_NAME STREQUAL "Linux")
     find_package(X11 REQUIRED)
-    find_package(Xrandr REQUIRED)
 
     find_package(PkgConfig REQUIRED)
     pkg_check_modules(XTST REQUIRED xtst)
 
-    target_include_directories(${PROJECT_NAME} PRIVATE
+    pkg_check_modules(XRANDR REQUIRED xrandr)
+
+
+    target_include_directories(LibIO
+            PUBLIC
             ${XTST_INCLUDE_DIRS}
     )
 
-    target_link_directories(${PROJECT_NAME} PRIVATE
+    target_link_directories(LibIO
+            PUBLIC
             ${XTST_LIBRARY_DIRS}
     )
 
